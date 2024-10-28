78106be8c7c8f5d9fcc8a50753eaca51d022cfae
Darci <darcinevitt@gmail.com>
2024-10-27 23:00:43 -0400

    resolving the Amount Due formatting issue

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -4,11 +4,11 @@
 
 SELECT
 customers.customer_name AS Customer,
-subscriptions.subscription_length*subscriptions.price_per_month AS Amount Due
+subscriptions.subscription_length*subscriptions.price_per_month AS "Amount Due"
 FROM orders
 LEFT JOIN customers ON orders.customer_id = customers.customer_id
 LEFT JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
 WHERE orders.order_status = 'unpaid'
 AND subscriptions.description = 'Fashion Magazine'
-GROUP BY customer_name
+GROUP BY customers.customer_name
 ORDER BY customer_name ASC;
